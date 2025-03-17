ef59e388769d8b844ae1fdb806ae321d9c59ef75
Vijay <138977952+Vijaykrishna31@users.noreply.github.com>
2025-03-17 13:20:36 +0530

    models files

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        clerkId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        currentSongId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Song",
+            default: null,
+        },
+    },
+    { timestamps: true } //  createdAt, updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
