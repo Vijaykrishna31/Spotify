5fed69a07c7a0a0c9ebb4dfb47bb9a78cb473277
Vijay <138977952+Vijaykrishna31@users.noreply.github.com>
2025-03-17 14:31:04 +0000

    App updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route
+                    path='/sso-callback'
+                    element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+                />
+                <Route path='/auth-callback' element={<AuthCallbackPage />} />
+                <Route path='/admin' element={<AdminPage />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+                <Route element={<MainLayout />}>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/chat' element={<ChatPage />} />
+                    <Route path='/albums/:albumId' element={<AlbumPage />} />
+                </Route>
+            </Routes>
+            <Toaster />
+        </>
+    );
+};
 
-export default App
+export default App;
